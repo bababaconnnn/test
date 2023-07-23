f243f8850f43429f82f71834219043c44e2d4884
Putita Techapat <putita@sirisoft.co.th>
2023-07-24 03:06:45 +0700

    update Coffee.java -> add desc

--- Coffee.java
+++ Coffee.java
@@ -1,6 +1,7 @@
 public class Coffee {
     String name;
     Integer amont;
+    String desc;
     public  Coffee(String name,Integer amont) {
         this.name = name;
         this.amont = amont;
