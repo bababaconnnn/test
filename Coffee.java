ccfcd9505d54fe43fb2e7624a5556c2937f8cf84
Putita Techapat <putita@sirisoft.co.th>
2023-07-24 00:55:46 +0700

    update Coffee.java add constructor to file

--- Coffee.java
+++ Coffee.java
@@ -1,6 +1,8 @@
 public class Coffee {
-
-    public  Coffee() {
-
+    String name;
+    Integer amont;
+    public  Coffee(String name,Integer amont) {
+        this.name = name;
+        this.amont = amont;
     }
 }
